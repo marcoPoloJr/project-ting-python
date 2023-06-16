c4fe10390d19ae90a1baa568f6afd8f4283db00d
Marco Polo <seguros.marcopolo@gmail.com>
2023-06-16 16:26:05 -0300

    Feat: req 03-Implemente a função processcapaz de transformar o conteúdo da lista gerada pela função txt_importer em um dicionário que será armazenado dentro da Queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,28 @@
+from operator import index
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        file = instance.search(i)
+        # print('FILE', file)
+        if file["nome_do_arquivo"] == path_file:
+            return
+
+    txtFile = txt_importer(path_file)
+    
+    dictionary = {
+        "nome_do_arquivo": path_file,
+        "qtq_linhas": len(txtFile),
+        "linhas_do_arquivo": txtFile
+    }
+
+    instance.enqueue(dictionary)
+    print(dictionary)
+    # print('Intance', instance.enqueue(dictionary))
+    # print('TXTFILE', txtFile)
 
 
 def remove(instance):
