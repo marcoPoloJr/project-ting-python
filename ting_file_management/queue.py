2efc94cffaeff57caf9875f043dc63b8b08a1b2a
Marco Polo <seguros.marcopolo@gmail.com>
2023-06-15 22:28:51 -0300

    fix: req 01-Correção linter

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,11 +1,14 @@
 """Linter"""
 from ting_file_management.abstract_queue import AbstractQueue
 
+
 class Queue(AbstractQueue):
     """Linter"""
+
     def __init__(self):
         """Inicialize sua estrutura aqui"""
-        self.queue= list()
+        self.queue = list()
+
     def __len__(self):
         """Aqui irá sua implementação"""
         return len(self.queue)
@@ -19,10 +22,9 @@ class Queue(AbstractQueue):
         if len(self.queue) == 0:
             return None
         return self.queue.pop(0)
-    
+
     def search(self, index):
         """Aqui irá sua implementação"""
         if index < 0 or index >= len(self.queue):
             raise IndexError("Índice Inválido ou Inexistente")
         return self.queue[index]
-
