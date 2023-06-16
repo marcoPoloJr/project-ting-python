78866f78230a726322f7f91299d282b42a7240c0
Marco Polo <seguros.marcopolo@gmail.com>
2023-06-16 16:53:13 -0300

    Feat: req 04-Implemente uma função remove capaz de remover o primeiro arquivo processado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,4 @@
-from operator import index
+"""Linter"""
 import sys
 from ting_file_management.file_management import txt_importer
 
@@ -11,22 +11,27 @@ def process(path_file, instance):
         if file["nome_do_arquivo"] == path_file:
             return
 
-    txtFile = txt_importer(path_file)
-    
+    txt_file = txt_importer(path_file)
+
     dictionary = {
-        "nome_do_arquivo": path_file,
-        "qtq_linhas": len(txtFile),
-        "linhas_do_arquivo": txtFile
+        'nome_do_arquivo': path_file,
+        'qtq_linhas': len(txt_file),
+        'linhas_do_arquivo': txt_file
     }
 
     instance.enqueue(dictionary)
-    print(dictionary)
+    print(dictionary, file=sys.stdout)
     # print('Intance', instance.enqueue(dictionary))
     # print('TXTFILE', txtFile)
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print('Não há elementos', file=sys.stdout)
+    file = instance.dequeue()
+    file_removed = file['nome_do_arquivo']
+    print(f'Arquivo {file_removed} removido com sucesso', file=sys.stdout)
 
 
 def file_metadata(instance, position):
