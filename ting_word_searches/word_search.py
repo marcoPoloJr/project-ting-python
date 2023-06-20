3624ecd648a3eceeca3ee0717bc5c25016329c12
Marco Polo <seguros.marcopolo@gmail.com>
2023-06-20 10:36:35 -0300

    Feat: req 07-Implemente uma função exists_word, que verifique a existência de uma palavra em todos os arquivos processados.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    result = list()
+
+    for i in range(0, len(instance)):
+        file = instance.search(i)
+        ocurrs = {
+            'palavra': word,
+            'arquivo': file['nome_do_arquivo'],
+            'ocorrencias': list()
+        }
+
+        for line, text in enumerate(file['linhas_do_arquivo']):
+            if word.casefold() in text.casefold():
+                ocurrs['ocorrencias'].append({'linha': line + 1})
+
+        if len(ocurrs['ocorrencias']) > 0:
+            result.append(ocurrs)
+            
+    return result
 
 
 def search_by_word(word, instance):
